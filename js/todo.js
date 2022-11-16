1c89f561c5ed1e282ed3214ec5c2a7054b65a042
OHDAL <yoonhj97@naver.com>
2022-11-16 17:16:43 +0900

    [MOD] todo 완료

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,30 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
-const TODOLIST_KEY = "todolist"
+const TODOLIST_KEY = "todolist";
 
-// const userTodoList = localStorage.getItem(TODOLIST_KEY) | [];
+let todos = JSON.parse(localStorage.getItem(TODOLIST_KEY)) || [];
+
+if (todos.length > 0) {
+  for (let i = 0; i < todos.length; i++) {
+    paintTodo(todos[i]);
+  }
+}
+
+function saveTodo(todo) {
+  if (todo) todos.push(todo);
+  console.log(todos);
+  localStorage.setItem(TODOLIST_KEY, JSON.stringify(todos));
+}
 
 function deleteTodo(e) {
   const li = e.target.parentElement;
+  const todoId = li.id.replace("todo-", "");
+
+  todos = todos.filter((item) => item.id != todoId);
+
   li.remove();
+  saveTodo();
 }
 
 function checkTodo(e) {
@@ -17,7 +34,7 @@ function checkTodo(e) {
   //span.style.textDecoration = "underline";
 }
 
-function paintTodo(todo) {
+function paintTodo({ id, todo, check }) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   const delBtn = document.createElement("button");
@@ -33,18 +50,20 @@ function paintTodo(todo) {
   li.appendChild(span);
   li.appendChild(chkBtn);
   li.appendChild(delBtn);
+  li.id = `todo-${id}`;
   todoList.appendChild(li);
-
-  userTodoList.push({text: todo, check: false});
-  localStorage.setItem(TODOLIST_KEY, userTodoList)
 }
 
 function handleToDoSubmit(e) {
   e.preventDefault();
 
-  const newTodo = todoInput.value;
+  const id = todos.length > 0 ? todos[todos.length - 1].id + 1 : 1;
+  const newTodo = { id, todo: todoInput.value, check: false };
+
   todoInput.value = "";
+
   paintTodo(newTodo);
+  saveTodo(newTodo);
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
