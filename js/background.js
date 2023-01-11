df3bb710a5ded739488b93de84bfa24b3225663c
윤현정 <yoonhj97@naver.com>
2023-01-12 05:31:03 +0900

    [ADD] background 이미지 클릭시 랜덤변경

--- js/background.js
+++ js/background.js
@@ -1,11 +1,31 @@
 const IMAGE_COUNT = 3
+let currentImage = null
 
-const chosenImage = Math.floor(Math.random() * IMAGE_COUNT);
+function randomiIndex(cnt) {
+    return Math.floor(Math.random() * cnt);
+}
 
+function setBackground(idx) {
+    currentImage = idx;
+    document.body.background = `./images/background/bg_${idx}.jpg`
+}
+
+// 이미지 Element 생성해서 body에 child로 붙이기 (강의에서) 
 // const bgImage = document.createElement("img");
 // bgImage.src = `images/bg_${chosenImage}.jpg`
 // bgImage.id = "background"
 
 // document.body.appendChild(bgImage)
 
-document.body.background = `./images/bg_${chosenImage}.jpg`
+document.body.addEventListener("click", (e) => {
+    if (e.target.tagName === "BODY") {
+        let temp = randomiIndex(IMAGE_COUNT);
+        while (chosenImage === temp) {
+            temp = randomiIndex(IMAGE_COUNT);
+        }
+
+        setBackground(temp);
+    }
+})
+
+setBackground(randomiIndex(IMAGE_COUNT));
